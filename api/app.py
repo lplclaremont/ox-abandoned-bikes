6ca78fba11df498a27694ff71ddf3c95af4b5428
Louis Claremont <lpclaremont@gmail.com>
2023-08-29 16:28:44 +0100

    get location/id route added to flask application

--- api/app.py
+++ api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from lib.location_repository import LocationRepository
 from lib.database_connection import DatabaseConnection
 import json
@@ -8,22 +8,30 @@ app = Flask(__name__)
 connector = DatabaseConnection()
 connector.connect('abandoned_bikes')
 
+def to_dict_form(location):
+    return {
+        "id": location.id,
+        "name": location.name,
+        "latitude": float(location.latitude),
+        "longitude": float(location.longitude)
+    }
+    
 @app.get("/")
 def home():
     return "Hello world!"
 
 @app.get("/locations")
-def locations():
+def get_locations():
     repository = LocationRepository(connector)
 
-    def to_json_form(location):
-        return {
-            "id": location.id,
-            "name": location.name,
-            "latitude": float(location.latitude),
-            "longitude": float(location.longitude)
-        }
+    locations = list(map(to_dict_form, repository.all()))
     
-    locations = list(map(to_json_form, repository.all()))
-    
-    return json.dumps(locations)
+    return jsonify(locations), 200
+
+@app.get("/locations/<int:location_id>")
+def get_location_by_id(location_id):
+    repository = LocationRepository(connector)
+
+    location = to_dict_form(repository.find(location_id))
+
+    return jsonify(location), 200
