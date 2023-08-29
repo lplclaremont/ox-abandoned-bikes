fd24d96b412cc76c2657cee4eca61b040bcdba43
Louis Claremont <lpclaremont@gmail.com>
2023-08-29 12:34:16 +0100

    all method for location repository class test driven

--- api/lib/location_repository.py
+++ api/lib/location_repository.py
@@ -6,6 +6,7 @@ class LocationRepository:
     def __init__(self, connection):
         self._connection = connection
     
+    # Returns a list of all locations from database
     def all(self):
         rows = self._connection.execute('SELECT * FROM locations')
         locations = []
@@ -14,3 +15,21 @@ class LocationRepository:
             locations.append(item)
 
         return locations
+    
+    # Returns a single location given the location ID
+    def find(self, location_id):
+        rows = self._connection.execute('SELECT * FROM locations WHERE id = %s', [location_id])
+        row = rows[0]
+
+        return Location(row["id"], row["name"], row["latitude"], row["longitude"])
+    
+    # Adds new location to the database
+    def create(self, location):
+        self._connection.execute('INSERT INTO locations (name, latitude, longitude) VALUES (%s, %s, %s)',
+                                 [location.name, location.latitude, location.longitude])     
+        return None
+        
+    # Removes location from database given the location ID
+    def delete(self, location_id):
+        self._connection.execute('DELETE FROM locations WHERE id = %s', [location_id])
+        return None
--- api/tests/test_location_repository.py
+++ api/tests/test_location_repository.py
@@ -20,3 +20,45 @@ def test_get_all_records(db_connection): # See conftest.py to learn what `db_con
         Location(1, "Rad Cam", Decimal('51.750000'), Decimal('-1.250000')),
         Location(2, "Westgate", Decimal('51.750000'), Decimal('-1.260000'))
     ]
+
+"""
+When we call LocationRepository#find
+We get a single a Location object reflecting seed data.
+"""
+def test_get_single_record(db_connection):
+    db_connection.seed("seeds/abandoned_bikes.sql")
+    repository = LocationRepository(db_connection)
+
+    location = repository.find(2)
+    assert location == Location(2, "Westgate", Decimal('51.750000'), Decimal('-1.260000'))
+
+"""
+When we call LocationRepository#create
+We create a new record in the database
+"""
+def test_create_record(db_connection):
+    db_connection.seed("seeds/abandoned_bikes.sql")
+    repository = LocationRepository(db_connection)
+
+    repository.create(Location(None, "Cornmarket", 1.111111, 2.222222))
+
+    result = repository.all()
+    assert result == [
+        Location(1, "Rad Cam", Decimal('51.750000'), Decimal('-1.250000')),
+        Location(2, "Westgate", Decimal('51.750000'), Decimal('-1.260000')),
+        Location(3, "Cornmarket", Decimal('1.111111'), Decimal('2.222222'))
+    ]
+
+"""
+When we call LocationRepository#delete
+We remove a record from thhe database.
+"""
+def test_delete_record(db_connection):
+    db_connection.seed("seeds/abandoned_bikes.sql")
+    repository = LocationRepository(db_connection)
+    repository.delete(1)
+
+    result = repository.all()
+    assert result == [
+        Location(2, "Westgate", Decimal('51.750000'), Decimal('-1.260000')),
+    ]
