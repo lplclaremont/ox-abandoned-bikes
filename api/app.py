e06531709629e519a6e51dfb29ec21afdfd28efc
Louis Claremont <lpclaremont@gmail.com>
2023-08-29 17:00:46 +0100

    post /location and delete /location added to application

--- api/app.py
+++ api/app.py
@@ -1,19 +1,19 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from lib.location_repository import LocationRepository
+from lib.location import Location
 from lib.database_connection import DatabaseConnection
-import json
 
 app = Flask(__name__)
 
 connector = DatabaseConnection()
 connector.connect('abandoned_bikes')
 
-def to_dict_form(location):
+def location_to_dict(location):
     return {
         "id": location.id,
         "name": location.name,
-        "latitude": float(location.latitude),
-        "longitude": float(location.longitude)
+        "latitude": location.latitude,
+        "longitude": location.longitude
     }
     
 @app.get("/")
@@ -23,15 +23,32 @@ def home():
 @app.get("/locations")
 def get_locations():
     repository = LocationRepository(connector)
-
-    locations = list(map(to_dict_form, repository.all()))
+    locations = list(map(location_to_dict, repository.all()))
     
     return jsonify(locations), 200
 
 @app.get("/locations/<int:location_id>")
 def get_location_by_id(location_id):
     repository = LocationRepository(connector)
-
-    location = to_dict_form(repository.find(location_id))
+    location = location_to_dict(repository.find(location_id))
 
     return jsonify(location), 200
+
+@app.post("/locations")
+def create_location():
+    data = request.get_json()
+    name = data["name"]
+    lat = data["latitude"]
+    long = data["longitude"]
+
+    repository = LocationRepository(connector)
+    repository.create(Location(None, name, lat, long))
+    
+    return jsonify({"status": "OK"}), 200
+
+@app.delete("/locations/<int:location_id>")
+def delete_location(location_id):
+    repository = LocationRepository(connector)
+    repository.delete(location_id)
+
+    return jsonify({"status": "OK"}), 200
