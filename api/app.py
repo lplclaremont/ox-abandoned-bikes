f65c0068fc683454dbafdd83a55f4a387257e6c0
Louis Claremont <lpclaremont@gmail.com>
2023-08-29 17:22:53 +0100

    get, post and delete /bikes endpoints added to flask application

--- api/app.py
+++ api/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request
-from lib.location_repository import LocationRepository
 from lib.location import Location
+from lib.location_repository import LocationRepository
+from lib.bike import Bike
+from lib.bike_repository import BikeRepository
 from lib.database_connection import DatabaseConnection
 
 app = Flask(__name__)
@@ -14,6 +16,16 @@ def location_to_dict(location):
         "name": location.name,
         "latitude": location.latitude,
         "longitude": location.longitude
+    }
+
+def bike_to_dict(bike):
+    return {
+        "id": bike.id,
+        "brand": bike.brand,
+        "colour": bike.colour,
+        "condition": bike.condition,
+        "date_found": bike.date_found,
+        "location_id": bike.location_id
     }
     
 @app.get("/")
@@ -43,12 +55,48 @@ def create_location():
 
     repository = LocationRepository(connector)
     repository.create(Location(None, name, lat, long))
-    
+
     return jsonify({"status": "OK"}), 200
 
 @app.delete("/locations/<int:location_id>")
 def delete_location(location_id):
     repository = LocationRepository(connector)
     repository.delete(location_id)
+
+    return jsonify({"status": "OK"}), 200
+
+
+@app.get("/bikes")
+def get_bikes():
+    repository = BikeRepository(connector)
+    bikes = list(map(bike_to_dict, repository.all()))
+    
+    return jsonify(bikes), 200
+
+@app.get("/bikes/<int:bike_id>")
+def get_bike_by_id(bike_id):
+    repository = BikeRepository(connector)
+    location = bike_to_dict(repository.find(bike_id))
+
+    return jsonify(location), 200
+
+@app.post("/bikes")
+def create_bike():
+    data = request.get_json()
+    brand = data["brand"]
+    colour = data["colour"]
+    condition = data["condition"]
+    date_found = data["date_found"]
+    location_id = data["location_id"]
+
+    repository = BikeRepository(connector)
+    repository.create(Bike(None, brand, colour, condition, date_found, location_id))
+
+    return jsonify({"status": "OK"}), 200
+
+@app.delete("/bikes/<int:bike_id>")
+def delete_bike(bike_id):
+    repository = BikeRepository(connector)
+    repository.delete(bike_id)
 
     return jsonify({"status": "OK"}), 200
